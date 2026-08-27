ba9cad9417e0dbe64847618a952fbd0ca3c3bf7b
pswjr0426-del <pswjr0426@gmail.com>
2026-08-27 13:10:22 +0900

    Update server.js

--- Race/server.js
+++ Race/server.js
@@ -56,4 +56,8 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 10000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
